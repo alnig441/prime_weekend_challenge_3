61ba7824007df0e234d3f055cd7fd270645f63fd
alnig441 <alnig441@gmail.com>
2015-08-28 19:24:09 -0500

    views served staticly

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+//var routes = require('./routes/index');
+var index = require('./routes/index');
 var app = express();
 
 // view engine setup
@@ -21,9 +20,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
