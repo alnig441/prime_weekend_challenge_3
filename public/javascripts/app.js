000478e79c8882aa1d77d57a1be71f5b82ca2baa
alnig441 <alnig441@gmail.com>
2015-08-29 11:03:57 -0500

    handlebars functionality corrected, loading to DOM succesfully

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -14,6 +14,19 @@ function getTeamMember(){
 
     }).done(function(data){
         console.log('AJAX call succeeded with ',data);
+        var team = data;
+        var position = $('.deltaTeam');
+
+        team.forEach(function(elem,index){
+            console.log(elem);
+            var teamMember = elem;
+            //var $newP = $('<p>');
+            //$newP.text(teamMember.name + '\n' + teamMember.shoutOut);
+            //console.log($newP);
+            //$('#intro').append($newP);
+            console.log(index);
+            $(position).append(template(teamMember));
+        })
 
     }).fail(function(jqXHR, textStatus, errorThrown){
         console.log('AJAX call to deltaCohor.json failed with error message: ',errorThrown);
